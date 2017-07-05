843bef00c126f01021c671a3bd55138aad682bfa
raghibr01 <raghibr01@gmail.com>
2017-07-06 01:03:27 +0530

    global variable file initiated

---
+++ constants.py
@@ -0,0 +1,5 @@
+#global variable files
+#INSTA API ACCESS TOKEN
+APP_ACCESS_TOKEN ='1745713605.ba27658.1bd8bacb6a6e455ab61bf76b0910481a'
+#INSTAGRAM BASE URL
+BASE_URL = 'https://api.instagram.com/v1'
