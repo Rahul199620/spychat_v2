67bb4e179c43ddf3db41f0ff0b0f22610ea4a835
raghibr01 <raghibr01@gmail.com>
2017-07-09 12:13:15 +0530

    self_info modified

--- self_info.py
+++ self_info.py
@@ -6,4 +6,17 @@ def self_info():
     request_url = (BASE_URL + 'users/self/?access_token=%s') % (APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
     user_info = requests.get(request_url).json()
-    
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            print 'username: %s' % (user_info['data']['username'])
+            print 'no of followers: %s' % (user_info['data']['counts']['followed_by'])
+            print 'no of people following you %s' % (user_info['data']['counts']['follows'])
+            print 'no of posts %s' % (user_info['data']['counts']['media'])
+        else:
+            print 'user does not exist'
+
+    else:
+        print 'status code other than 200 revieved'
+
+
+
