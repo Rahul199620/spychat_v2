a1408e6654da6da5eac23a24106c6f705b0c06b2
raghibr01 <raghibr01@gmail.com>
2017-07-12 00:38:11 +0530

    comment ho ri hai

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from self_info import self_info
 from get_own_post import get_own_post
 from get_user_info import get_user_info
 from get_user_post import get_user_post
+#from select_a_username import select_a_username
 #fetching own info
 #self_info()
 #get_own_post()
-get_user_post()
+#select_a_username()
+insta_username=raw_input("enter user name")
+get_user_post(insta_username)
 #search_post_by_choice()
