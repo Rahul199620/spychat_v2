525ab63794f6ed448fe5c0b56862eca5ac6b8437
raghibr01 <raghibr01@gmail.com>
2017-07-11 12:41:18 +0530

    get own post and self info modified again

--- get_own_post.py
+++ get_own_post.py
@@ -1,4 +1,5 @@
 import requests
+import json
 import urllib
 from constants import APP_ACCESS_TOKEN , BASE_URL
 def get_own_post():
@@ -6,12 +7,17 @@ def get_own_post():
     request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
     own_media = requests.get(request_url).json()
+    print json.dumps(own_media, indent=4)
     if own_media['meta']['code'] == 200:
         if len(own_media['data']):
             image_name = own_media['data'][0]['id'] + '.jpeg'
+            video_new = own_media['data'][0]['id'] + '.mp4'
             image_url = own_media['data'][0]['images']['standard_resolution']['url']
+            video_url = own_media ['data'] [0] ['videos']['standard_resolution']['url']
             urllib.urlretrieve(image_url, image_name)
+            urllib.urlretrieve(video_url, video_new)
             print 'Your image has been downloaded!'
+            print 'your video has been downloaded'
         else:
             print 'Post does not exist!'
     else:
